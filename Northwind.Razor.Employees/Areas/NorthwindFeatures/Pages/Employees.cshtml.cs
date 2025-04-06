21f13f254ffd4fe1623c258e69bc39868f007d08
Elie898 <elie_008@hotmail.com>
2025-04-06 02:38:17 +0200

    Razor klass bibliotek för employees

---
+++ Northwind.Razor.Employees/Areas/NorthwindFeatures/Pages/Employees.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Northwind.Razor.Employees.MyFeature.Pages;
+
+public class EmployeesModel : PageModel
+{
+    public void OnGet()
+    {
+        ViewData["Title"] = "Northwind App - Employees";
+    }
+}
