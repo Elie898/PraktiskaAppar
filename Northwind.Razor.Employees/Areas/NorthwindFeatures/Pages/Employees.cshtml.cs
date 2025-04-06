ddc183a998f01a1f52d3dea11a7351044c118402
Elie898 <elie_008@hotmail.com>
2025-04-06 03:02:14 +0200

    Partial view - Employee

--- Northwind.Razor.Employees/Areas/NorthwindFeatures/Pages/Employees.cshtml.cs
+++ Northwind.Razor.Employees/Areas/NorthwindFeatures/Pages/Employees.cshtml.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Northwind.EntityModels;
 
 namespace Northwind.Razor.Employees.MyFeature.Pages;
 
 public class EmployeesModel : PageModel
 {
+    private NorthwindDatabaseContext db;
+    public EmployeesModel(NorthwindDatabaseContext injectedContext)
+    {
+        db = injectedContext;
+    }
+    public Employee[] Employees { get; set; } = null!;
     public void OnGet()
     {
         ViewData["Title"] = "Northwind App - Employees";
+        Employees = db.Employees.OrderBy(e => e.LastName)
+                 .ThenBy(e => e.FirstName).ToArray();
     }
 }
